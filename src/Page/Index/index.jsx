d09bae4cb5409a4e8f3a0f5b03234fde444a3da4
吴鑫博 <wuxinbo@wuxinbodeMacBook-Pro.local>
2020-04-09 00:27:11 +0800

    HTML5API联系

--- src/Page/Index/index.jsx
+++ src/Page/Index/index.jsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import {
-  useHistory,
-} from "react-router-dom";
+import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
+import _ from 'lodash';
 import { Layout, Menu, Breadcrumb, Button } from 'antd';
+import styles from './style.module.less';
 const { Header, Content, Footer } = Layout;
 
 const Index = (props) => {
+  const [location, setLocation] = useState('');
   let history = useHistory();
   const handleEntryAdmin = () => {
     history.push('/admin');
   }
 
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      // console.log('navigator.geolocation:', navigator.geolocation.)
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          console.log('position:', position);
+          // const { } = position
+          const latitude = _.get(position, 'coords.latitude', '');
+          const longitude = _.get(position, 'coords.longitude', '');
+          const location = `经度:${longitude}-纬度:${latitude}`;
+          setLocation(location);
+        },
+        (error) => {
+          console.log('error:', error);
+        },
+      )
+    } else {
+      console.log('Geolocation is not supported by this browser.');
+    }
+  }
+
+  // const showPosition = 
+
   return (
-    <Layout className="layout">
-      <Header>
+    <Layout className={styles.layout}>
+      <Header className={styles.header}>
         <div className="logo" />
         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
           <Menu.Item key="1">nav 1</Menu.Item>
           <Menu.Item key="2">nav 2</Menu.Item>
           <Menu.Item key="3">nav 3</Menu.Item>
         </Menu>
-        <Button type="link" onClick={handleEntryAdmin}>控制台</Button>
+        <Button className={styles.right} type="link" onClick={handleEntryAdmin}>控制台</Button>
       </Header>
       <Content style={{ padding: '0 50px' }}>
         <Breadcrumb style={{ margin: '16px 0' }}>
@@ -29,6 +53,10 @@ const Index = (props) => {
           <Breadcrumb.Item>App</Breadcrumb.Item>
         </Breadcrumb>
         <div className="site-layout-content">
+        </div>
+        <Button type="primary" onClick={getLocation}>获取当前位置</Button>
+        <div>
+          <span>{location}</span>
         </div>
       </Content>
       <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
--- src/Page/Overview/index.jsx
+++ src/Page/Overview/index.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Button } from 'antd';
 
 const Index = (props) => {
-  return <div>Overview</div>
+  console.log(window.location);
+
+  const handleReload = () => {
+    window.location.reload();
+  }
+
+  const handleGetSearch = () => {
+    const { search } = window.location;
+    console.log('search:', search);
+  }
+
+  const handleNavigator = () => {
+    console.log('navigator:', navigator);
+  }
+
+  return (
+    <div>
+      Overview
+      <Button type="primary" onClick={handleReload}>Reload</Button>
+      <Button type="primary" onClick={handleGetSearch}>GetSearch</Button>
+      <Button type="primary" onClick={handleNavigator}>Navigator</Button>
+    </div>
+  )
 }
 
 export default Index;
